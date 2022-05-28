13c7905ed8a062f7f7c2628d1c7aa463e1a5b17a
nozymech <sjyang.pt@gmail.com>
2022-05-28 22:59:23 +0800

    fix the calculator

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 let displayValue = "";
-// store input value
+// input value sandbox
 let numbers = [];
-//store operate type
-let operation = "";
+//operate type sandbox
+let operation = [];
 const int = document.getElementById("int")
 int.innerText = displayValue;
 const ac = document.getElementById("ac")
 const numberButtons = document.querySelectorAll("button.num")
 const operateButtons = document.querySelectorAll("button.operator")
 const equal = document.getElementById("equal")
+const minus = document.getElementById("minus")
+const percentage = document.getElementById("percentage")
 
+// click AC: reset the calculator
 ac.addEventListener("click",()=>{
 	displayValue = "";
 	int.innerText = displayValue;
 	numbers = [];
 })
 
+// click numbers: add value
 numberButtons.forEach((button)=>{
 	button.addEventListener('click',()=>{
 		displayValue = displayValue + button.value
@@ -24,30 +28,81 @@ numberButtons.forEach((button)=>{
 	})
 })
 
+//click operate buttons : 1. save value and wait for another value to calculation 
 operateButtons.forEach((button)=>{
 	button.addEventListener('click',()=>{
-		operation = button.id;
+		// save the first target button id 
+		operation.push(button.id);
+		// put value into sandbox 
 		numbers.push(displayValue);
+		// refresh the screen 
 		displayValue = "";
 		int.innerText = displayValue;
+
+		// if we have 2 value in array wii do calculate 
+		if (numbers.length == 2) {
+	if(operation[0] == "add"){
+		add(Number(numbers[0]),Number(numbers[1]));
+		// reset input value sandbox
+		numbers = [];
+		// reset operate sandbox
+		operation = [];
+	}else if(operation[0] == "subtract") {
+		subtract(Number(numbers[0]),Number(numbers[1]));
+		numbers = [];
+		operation = [];
+	}else if(operation[0] == "multiply") {
+		multiply(Number(numbers[0]),Number(numbers[1]));
+		numbers = [];
+		operation = [];
+	}else if(operation[0] == "divide") {
+		divide(Number(numbers[0]),Number(numbers[1]));
+		numbers = [];
+		operation = [];
+	}
+		}else if (numbers.length == 1){
+			int.innerText = displayValue;
+		}
 	})
 })
 
 equal.addEventListener("click",()=>{
 	numbers.push(displayValue);
-	if(operation == "add"){
-		add(parseInt(numbers[0]),parseInt(numbers[1]));
-		numbers = [];	
-	}else if(operation == "minus") {
-		subtract(parseInt(numbers[0]),parseInt(numbers[1]));
-		numbers = [];
-	}else if(operation == "multiply") {
-		multiply(parseInt(numbers[0]),parseInt(numbers[1]));
-		numbers = [];
-	}else if(operation == "divide") {
-		divide(parseInt(numbers[0]),parseInt(numbers[1]));
-		numbers = [];
+	if(operation.length >0){
+		if(operation[0] == "add"){
+			add(Number(numbers[0]),Number(numbers[1]));
+			// save result into sandbox ,and not change second value 
+			numbers[0] = displayValue;
+	
+	
+		}else if(operation[0] == "subtract") {
+			subtract(Number(numbers[0]),Number(numbers[1]));
+			numbers[0] = displayValue;
+	
+		}else if(operation[0] == "multiply") {
+			multiply(Number(numbers[0]),Number(numbers[1]));
+			numbers[0] = displayValue;
+	
+		}else if(operation[0] == "divide") {
+			divide(Number(numbers[0]),Number(numbers[1]));
+			numbers[0] = displayValue;
+	
+		}
+	}else {
+		int.innerText = displayValue;
 	}
+})
+
+minus.addEventListener("click",()=>{
+	numbers.push(displayValue);
+	negative(numbers[0])
+	numbers = [];
+})
+
+percentage.addEventListener("click",()=>{
+	numbers.push(displayValue);
+	percent(numbers[0])
+	numbers = [];
 })
 
 
@@ -78,14 +133,13 @@ const divide = function(a, b) {
 
 
 const negative = function(a) {
-	return -a ;
+	displayValue = -a  ;
+	int.innerText = displayValue;
+	numbers = [];
 };
 
 
 const percent = function(a) {
-    return a/100;
-}
-
-function operate(a,b) {
-	int.innerText = result;
+    displayValue = a/100  ;
+	int.innerText = displayValue;
 }
